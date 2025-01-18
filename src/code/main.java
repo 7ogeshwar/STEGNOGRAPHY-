2b77bc7a9257533a5926f4a30707f419df905b49
7ogeshwar <logeshwra516@gmail.com>
2025-01-18 17:05:14 +0530

    final
    	new file:   .classpath
    	new file:   .project
    	new file:   README.md
    	new file:   build.xml
    	new file:   build/classes/code/encode$1.class
    	new file:   build/classes/code/encode.form

---
+++ src/code/main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package code;
+
+/**
+ *
+ * @author logig
+ */
+public class main {
+    public static void main(){
+    mainmenu mm  = new mainmenu();
+    mm.setVisible(true);
+    mm.setLocationRelativeTo(null);
+    
+    }
+}
